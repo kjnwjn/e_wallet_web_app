767a3e6eea5f4f286d681769119a858766b6ce1e
truonggiangit793 <truonggiangit793@gmail.com>
2022-03-02 02:38:10 +0700

    Setup enviroment variables

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+require_once('./private/core/DevCode.php');
 require_once('./private/bridge.php');
-$_SERVER['HTTP_HOST'] = 'http://localhost/live/';
+
+use DevCoder\DotEnv;
+
+(new DotEnv(__DIR__ . '/.env'))->load();
+
 $myApp = new App();
